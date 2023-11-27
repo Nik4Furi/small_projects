a35ee6b3037a72b6fb470d911040a170678f0995
Nik4Furi <nikhilgurjar2212@gmail.com>
2023-11-27 20:15:41 +0530

    Start to doing heap ...

--- 02_Heap/01_Implementation.cpp
+++ 02_Heap/01_Implementation.cpp
@@ -90,11 +90,46 @@ public:
         }
     }
 
-    //-----------function to implement arr into heapify
-    
-
 
 };
+    //-----------function to implement arr into heapify
+void heapify(int arr[],int n,int i){ // we make to try to build a heap from this index
+// to take their right place take log n time, but building a heap, that will take n time, tC (nlogn)
+
+    int largest  = i;
+    int left = 2*i,right = 2*i+1;
+
+    if(largest <=n && arr[largest] < arr[left])
+        largest = left;
+    
+    if(largest <=n && arr[largest] < arr[right])
+        largest = right;
+
+    if(largest != i){
+        swap(arr[i],arr[largest]);
+        heapify(arr,n,largest);
+    }
+
+}
+
+
+//--------------- Function to implement heap sort
+void heapSort(int arr[],int n){
+    int t = n;
+
+    while(t > 1){
+        swap(arr[t],arr[1]);
+
+        t--;
+
+        heapify(arr,n,1);
+    }
+}
+
+
+/*
+Priority Queue, is used to implement max or min heap help of STL
+*/
 
 int main()
 {
@@ -109,15 +144,32 @@ int main()
 
 
     cout << "Print heap "<<endl;
-    h.print();
+    // h.print();
 
     h.deleteNode();
 
 
     cout << "Print heap after delete "<<endl;
-    h.print();
+    // h.print();
 
-    
+    int arr[6] = {-1,54,55,53,52,50};
+    int n = 6;
+    for(int i=n/2;i>0;i--)
+        heapify(arr,n,i);
+
+    for(int i=1;i<=n;i++)
+        cout << arr[i] << " ";
+    cout <<endl;
+
+    cout <<endl;
+
+    cout << "Soted array" <<endl;
+
+    heapSort(arr,n);
+
+    for(int i=1;i<=n;i++)
+        cout << arr[i] << " ";
+    cout <<endl;
 
     return 0;
 }
